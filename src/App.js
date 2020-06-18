590f68c21300e22ceb6ab8c8a3c37ec73a01a1a0
Helegbedey <devaze007@gmail.com>
2020-06-18 23:14:23 +0100

    added webpack css-loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   // eslint-disable-next-line class-methods-use-this
   render() {
     return (
-            <div>Hello world</div>
+            <div classsName='app'>Hello world</div>
     );
   }
 }
